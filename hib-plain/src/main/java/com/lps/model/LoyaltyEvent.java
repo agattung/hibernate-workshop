b1ff7df56c6d0423c89d5e83af2102f763ef5dae
Armin Gattung <armin.gattung@groupeetix.com>
2014-05-24 09:59:37 +0200

    Good old Hibernate at work

---
+++ hib-plain/src/main/java/com/lps/model/LoyaltyEvent.java
@@ -0,0 +1,35 @@
+package com.lps.model;
+
+public class LoyaltyEvent {
+	
+	private long id;
+
+	private int version;
+	
+	private String whatHappened;
+
+	void setId(long id) {
+		this.id = id;
+	}
+		
+	public long getId() {
+		return id;
+	}
+
+	void setVersion(int version) {
+		this.version = version;
+	}
+	public int getVersion() {
+		return version;
+	}
+
+
+	public String getWhatHappened() {
+		return whatHappened;
+	}
+
+	public void setWhatHappened(String whatHappened) {
+		this.whatHappened = whatHappened;
+	}	
+	
+}
---
+++ hib-plain/src/test/java/com/lps/EntityTest.java
@@ -0,0 +1,111 @@
+package com.lps;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.junit.After;
+import org.junit.Assert;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.lps.model.LoyaltyEvent;
+
+public class EntityTest {
+
+	private static SessionFactory sessionFactory;
+
+	private Session session;
+	
+	@BeforeClass
+	public static void setUpClass() {
+		try {
+			sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Unexpected exception");			
+		}
+	}
+	
+	@Before
+	public void setUp() {
+		session = sessionFactory.openSession();		
+	}
+
+	@After
+	public void tearDown() {
+		if (session != null) {
+			session.close();
+		}
+	}
+	
+	@Test
+	public void test01_createEvent() {
+		newEvent();
+	}
+		
+	@Test
+	public void test02_updateEvent() {
+		LoyaltyEvent event = newEvent();
+		Transaction tx = session.beginTransaction();
+		
+		// Hibernate detects unchanged objects
+		String oldHappening = event.getWhatHappened();
+		String newHappening = "Hibernate workshop continued";
+		event.setWhatHappened(newHappening);
+		event.setWhatHappened(oldHappening);
+
+		
+		session.save(event);
+		assertEquals(0, event.getVersion());
+		tx.commit();
+		assertEquals(0, event.getVersion());
+		assertTrue(session.contains(event));
+		
+		session = sessionFactory.openSession();
+		assertFalse(session.contains(event));
+		tx = session.beginTransaction();
+		event = (LoyaltyEvent) session.merge(event);
+		
+		// "implicit" persistence, no additional save necessary
+		// flush increases the version number
+		event.setWhatHappened(newHappening);
+
+		session.flush();
+		assertTrue(event.getVersion() > 0 );
+		
+		tx.commit();
+
+		long eventId = event.getId();
+		
+		session.close();
+		
+		session = sessionFactory.openSession();
+		assertFalse(session.contains(event));
+		
+		event = (LoyaltyEvent) session.get(LoyaltyEvent.class, eventId);
+		assertEquals(newHappening, event.getWhatHappened());
+	}
+	
+	private LoyaltyEvent newEvent() {
+		Transaction tx = session.beginTransaction();
+		
+		LoyaltyEvent event = new LoyaltyEvent();
+		event.setWhatHappened("Hibernate workshop started");
+		assertEquals(0, event.getId());
+		assertEquals(0, event.getVersion());
+		
+		// save sets the id
+		session.save(event);
+		long eventId = event.getId();
+		assertTrue(eventId > 0 );
+		tx.commit();
+
+		assertTrue(event.getId() > 0 );
+		assertEquals(0, event.getVersion());
+		return event;
+	}
+
+}
